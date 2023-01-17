b301d24041e76f222b14b2c1b5a568811f190e5f
Tom Cooper <cooper.tom.201@gmail.com>
2023-01-17 14:42:59 +0000

    matlapplots

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-print("test")
+import random as nr
+import matplotlib.pyplot as plt
+
+def sim_randomWalk(num):
+    value = [0]
+    for i in range(1, num+1):
+        choice = nr.uniform(0,1)
+        if choice> 0.5:
+            value.append(value[i-1]+1)
+        else:
+            value.append(value[i-1]-1)
+    #plot
+    fig = plt.figure(1, figsize=(9, 6))
+    ax = fig.gca()
+    plt.plot(value)
+    ax.set_title('Simple Random Walk with ' + str(num) + ' steps')
+    ax.set_ylabel('Position')
+    ax.set_xlabel('steps')
+    plt.show()
+    return 'Done!'
+sim_randomWalk(1000)
+
